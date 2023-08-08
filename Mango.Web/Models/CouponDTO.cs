b903f7a6249b4aa83feea67eaf37f304ee360e11
aveesh dilaksha <adilaksha@jitsy.dev>
2023-08-08 15:21:20 +0530

    Mango.Web - Added Coupon Service in the services folder and created all the crud operations
    added coupon url in the appsettings.json and connected them in program.cs

---
+++ Mango.Web/Models/CouponDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Web.Models
+{
+    public class CouponDTO
+    {
+        public int CouponId { get; set; }
+        public string? couponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
